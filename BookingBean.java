b0011578e8c6b8bb98b8111c5a36d2cb586aa264
yeeun <61838900+yechubot@users.noreply.github.com>
2020-12-10 18:02:56 +0900

    Add files via upload

---
+++ BookingBean.java
@@ -0,0 +1,29 @@
+package steakhouse;
+
+public class BookingBean {
+
+		private String name;
+		private String phone;
+		private String date;
+		
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public String getPhone() {
+			return phone;
+		}
+		public void setPhone(String phone) {
+			this.phone = phone;
+		}
+		public String getDate() {
+			return date;
+		}
+		public void setDate(String date) {
+			this.date = date;
+		}
+	
+
+}
---
+++ BookingMgr.java
@@ -0,0 +1,43 @@
+package steakhouse;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+public class BookingMgr {
+
+	private DBConnectionMgr pool;
+	
+	public BookingMgr() {
+		try {
+			pool = DBConnectionMgr.getInstance();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	//예약하기
+	public boolean insertBooking(BookingBean bean) {
+		Connection connection = null;
+		PreparedStatement statement = null;
+		String sql = null;
+		boolean flag = false;
+		try {
+			connection = pool.getConnection();
+			sql = "insert bookingtbl(name,phone,date) + values(?,?,?)";
+			statement = connection.prepareStatement(sql);
+			statement.setString(1, bean.getName());
+			statement.setString(2, bean.getPhone());
+			statement.setString(3, bean.getDate());
+		}catch(Exception e) {
+			e.printStackTrace();
+			
+		}finally {
+			pool.freeConnection(connection,statement);
+		}
+		return flag;
+	}
+
+}
+
+
+
